3199e77c7eb52bf10246a7ce8473b9597fdd1494
Abir <abir.hossain2107@gmail.com>
2022-11-12 11:52:22 +0600

    Created factory and creating flock

--- DuckSimulator/src/DuckSimulator.java
+++ DuckSimulator/src/DuckSimulator.java
@@ -1,21 +1,26 @@
 public class DuckSimulator {
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
-        simulator.simulate();
+        AbstractDuckFactory duckFactory = new CountingDuckFactory();
+        simulator.simulate(duckFactory);
     }
-    void simulate() {
-        Quackable mallardDuck = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
-        System.out.println("\nDuck Simulator");
+    void simulate(AbstractDuckFactory duckFactory) {
+        Quackable mallardDuck = duckFactory.createMallardDuck();
+        Quackable redheadDuck = duckFactory.createRedheadDuck();
+        Quackable duckCall = duckFactory.createDuckCall();
+        Quackable rubberDuck = duckFactory.createRubberDuck();
+        Quackable gooseDuck = new GooseAdapter(new Goose());
+        System.out.println("\nDuck Simulator: With Abstract Factory");
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
+        simulate(gooseDuck);
+        System.out.println("The ducks quacked " +
+                QuackCounter.getQuacks() +
+" times");
     }
     void simulate(Quackable duck) {
         duck.quack();
     }
-
 }
