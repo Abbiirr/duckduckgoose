b2a087e4cdd42802eeb66abdd72e9fc802d9ae95
Abir <abir.hossain2107@gmail.com>
2022-11-14 10:43:22 +0600

    Created flock using composite pattern

--- DuckSimulator/src/DuckSimulator.java
+++ DuckSimulator/src/DuckSimulator.java
@@ -5,18 +5,31 @@ public class DuckSimulator {
         simulator.simulate(duckFactory);
     }
     void simulate(AbstractDuckFactory duckFactory) {
-        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
-        System.out.println("\nDuck Simulator: With Abstract Factory");
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseDuck);
-        System.out.println("The ducks quacked " +
+        System.out.println("\nDuck Simulator: With Composite - Flocks");
+        Flock flockOfDucks = new Flock();
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+        Flock flockOfMallards = new Flock();
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+        flockOfDucks.add(flockOfMallards);
+        System.out.println("\nDuck Simulator: Whole Flock Simulation");
+        simulate(flockOfDucks);
+        System.out.println("\nDuck Simulator: Mallard Flock Simulation");
+        simulate(flockOfMallards);
+        System.out.println("\nThe ducks quacked " +
                 QuackCounter.getQuacks() +
 " times");
     }
